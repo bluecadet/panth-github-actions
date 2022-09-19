a0591795606ef20e91e2df51cf6f6c6b8df62fd0
Pete Inge <pinge@bluecadet.com>
2022-09-18 21:26:00 -0400

    oh variables

--- .ci/scripts/env/copy-to-bc-connect.sh
+++ .ci/scripts/env/copy-to-bc-connect.sh
@@ -6,6 +6,6 @@ UUID=4a1a251a-53ba-4793-a41f-862eb890934c
 ENV=live
 TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
 
-echo $(TIMESTAMP)
+echo $TIMESTAMP
 
 rsync -rLvz --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=../artifacts $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/vis-reg-reports/$TIMESTAMP
