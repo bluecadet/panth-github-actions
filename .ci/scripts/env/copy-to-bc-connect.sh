47a9289efd7c12c0c5b665c97ee6d1a667c42ae1
Pete Inge <pinge@bluecadet.com>
2022-09-18 21:23:43 -0400

    changing timestamp

--- .ci/scripts/env/copy-to-bc-connect.sh
+++ .ci/scripts/env/copy-to-bc-connect.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 UUID=4a1a251a-53ba-4793-a41f-862eb890934c
 ENV=live
-timestamp=$(date +%s)
+TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
 
-echo $(timestamp)
+echo $(TIMESTAMP)
 
-rsync -rLvz --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=../artifacts $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/vis-reg-reports/$timestamp
+rsync -rLvz --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=../artifacts $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/vis-reg-reports/$TIMESTAMP
