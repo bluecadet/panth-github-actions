8781fd389ad62ebdf683e8ac1e4d5fb8832c7687
Pete Inge <pinge@bluecadet.com>
2022-09-20 14:11:22 -0400

    Have to change back dirs

--- .ci/scripts/env/copy-to-connect-bc.sh
+++ .ci/scripts/env/copy-to-connect-bc.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
+PWD=$(pwd)
 UUID=4a1a251a-53ba-4793-a41f-862eb890934c
 ENV=live
 
@@ -16,16 +17,10 @@ cp -R ../artifacts ../to-be-copied/$TIMESTAMP
 cd ../to-be-copied
 rsync -raRLvz --relative --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=~/tmp/ $ENV.$UUID@appserver.$ENV.$UUID.drush.in:files/vis-reg-reports
 
+
+cd $PWD
 VR_PR_LINK="[VR Report](https://live-connect-bluecadet.pantheonsite.io/sites/default/files/vis-reg-reports/$TIMESTAMP/artifacts/backstop_data/html_report/index.html)"
 
 echo $VR_PR_LINK
 
 echo "[VR Report](https://live-connect-bluecadet.pantheonsite.io/sites/default/files/vis-reg-reports/$TIMESTAMP/artifacts/backstop_data/html_report/index.html)" >> message.md
-
-# VR_PR_MESSAGE="$VR_PR_MESSAGE<br><br>$VR_PR_LINK"
-
-# echo $VR_PR_MESSAGE
-
-# (
-#   echo "VR_PR_MESSAGE=$VR_PR_MESSAGE"
-# ) >> $GITHUB_ENV
