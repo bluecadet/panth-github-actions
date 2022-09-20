3e104935de6a7dbb3f238fff2c6775e3cffffbc9
Pete Inge <pinge@bluecadet.com>
2022-09-20 14:40:20 -0400

    should have figured that var was taken…

--- .ci/scripts/env/copy-to-connect-bc.sh
+++ .ci/scripts/env/copy-to-connect-bc.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-PWD=$(pwd)
+PWD_VAR=$(pwd)
 UUID=4a1a251a-53ba-4793-a41f-862eb890934c
 ENV=live
 
-echo $PWD
+echo $PWD_VAR
 
 curl -H "api-key: $CONNECT_BC_API" -d "project=$TERMINUS_SITE&branch=$GITHUB_REF_NAME" -X POST https://live-connect-bluecadet.pantheonsite.io/api/vis-reg-result >> connect-bc.js
 
@@ -20,9 +20,9 @@ cd ../to-be-copied
 rsync -raRLvz --relative --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=~/tmp/ $ENV.$UUID@appserver.$ENV.$UUID.drush.in:files/vis-reg-reports
 
 
-echo $PWD
+echo $PWD_VAR
 
-cd "$PWD"
+cd "$PWD_VAR"
 
 VR_PR_LINK="[VR Report](https://live-connect-bluecadet.pantheonsite.io/sites/default/files/vis-reg-reports/$TIMESTAMP/artifacts/backstop_data/html_report/index.html)"
 
