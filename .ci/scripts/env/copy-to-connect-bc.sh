2ad4e4b7bb835857f59de4ab37d4cbd7032dad4f
Pete Inge <pinge@bluecadet.com>
2022-09-20 12:25:53 -0400

    Working on message variables

--- .ci/scripts/env/copy-to-connect-bc.sh
+++ .ci/scripts/env/copy-to-connect-bc.sh
@@ -16,9 +16,13 @@ cp -R ../artifacts ../to-be-copied/$TIMESTAMP
 cd ../to-be-copied
 rsync -raRLvz --relative --size-only --checksum --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=~/tmp/ $ENV.$UUID@appserver.$ENV.$UUID.drush.in:files/vis-reg-reports
 
-VR_PR_LINK=[VR Report](https://live-connect-bluecadet.pantheonsite.io/sites/default/files/vis-reg-reports/$TIMESTAMP/artifacts/backstop_data/html_report/index.html)
+VR_PR_LINK="[VR Report](https://live-connect-bluecadet.pantheonsite.io/sites/default/files/vis-reg-reports/$TIMESTAMP/artifacts/backstop_data/html_report/index.html)"
 echo $VR_PR_LINK
 
+VR_PR_MESSAGE="$VR_PR_MESSAGE<br><br>$VR_PR_LINK"
+
+echo $VR_PR_MESSAGE
+
 (
-  echo "VR_PR_LINK=$VR_PR_LINK"
+  echo "VR_PR_MESSAGE=$VR_PR_MESSAGE"
 ) >> $GITHUB_ENV
