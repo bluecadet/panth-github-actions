4a57a4d6bf98248149bc2363aeb0618b3784674b
Pete Inge <pete.inge@gmail.com>
2024-10-21 15:06:18 -0400

    testing string split. and more debugging

--- .ci/scripts/pr-notifications.js
+++ .ci/scripts/pr-notifications.js
@@ -1,7 +1,11 @@
+const core = require('@actions/core');
 
 
 
 console.log(process.env);
+
+
+
 
 const CONTEXT_GITHUB = JSON.parse(process.env.CONTEXT_GITHUB);
 
@@ -13,11 +17,26 @@ const CONTEXT_GITHUB = JSON.parse(process.env.CONTEXT_GITHUB);
 // Opening a PR.
 if (process.env.GITHUB_EVENT_NAME == "pull_request" && process.env.GITHUB_EVENT_ACTION == "opened") {
   console.log(CONTEXT_GITHUB);
-  let current_description = CONTEXT_GITHUB.pull_request.body ?? "";
+  let current_description = CONTEXT_GITHUB.event.pull_request.body ?? "";
   console.log(current_description);
+
+  if (current_description) {
+    // MSG_SEPERATOR.
+    console.log(process.env.MSG_SEPERATOR);
+    console.log(current_description.split(process.env.MSG_SEPERATOR));
+  }
 }
 
 
 else {
   console.log("Sorry, I don't know what to do.");
+}
+
+
+
+try {
+  const myOutput = 'Hello from JavaScript!';
+  core.setOutput('myOutput', myOutput);
+} catch (error) {
+  core.setFailed(error.message);
 }
