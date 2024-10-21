70fb07c353f2a5e29492313e620accbb64942ecd
Pete Inge <pete.inge@gmail.com>
2024-10-21 13:55:16 -0400

    testing event name and type vars

--- .ci/scripts/pr-notifications.js
+++ .ci/scripts/pr-notifications.js
@@ -1,2 +1,22 @@
 
+
+
 console.log(process.env);
+
+const CONTEXT_GITHUB = JSON.parse(process.env.CONTEXT_GITHUB);
+
+// Determine action
+// GITHUB_EVENT_NAME
+// const action = CONTEXT_GITHUB.action;
+// console.log(action);
+
+// Opening a PR.
+if (process.env.GITHUB_EVENT_NAME == "pull_request" && process.env.GITHUB_EVENT_TYPE == "opened") {
+  let current_description = CONTEXT_GITHUB.body ?? "";
+  console.log(current_description);
+}
+
+
+else {
+  console.log("Sorry, I don't know what to do.");
+}
