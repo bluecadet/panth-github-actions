944238bb9a8cebb3876510db7b67b6b254ad38a4
Pete Inge <pete.inge@gmail.com>
2024-10-21 19:26:52 -0400

    Testing slack notification body creation

--- .ci/scripts/pr-notifications.js
+++ .ci/scripts/pr-notifications.js
@@ -1,3 +1,7 @@
+// Provide these outputs.
+// body_footer
+// slack payload
+
 const core = require('@actions/core');
 
 
@@ -15,22 +19,36 @@ const CONTEXT_GITHUB = JSON.parse(process.env.CONTEXT_GITHUB);
 // console.log(action);
 
 // Opening a PR.
-if (process.env.GITHUB_EVENT_NAME == "pull_request" && process.env.GITHUB_EVENT_ACTION == "opened") {
+// if (process.env.GITHUB_EVENT_NAME == "pull_request" && process.env.GITHUB_EVENT_ACTION == "opened") {
   console.log(CONTEXT_GITHUB);
   let current_description = CONTEXT_GITHUB.event.pull_request.body ?? "";
   console.log(current_description);
+  let body = "";
+  let data = "";
 
   if (current_description) {
     // MSG_SEPERATOR.
     console.log(process.env.MSG_SEPERATOR);
     console.log(current_description.split(process.env.MSG_SEPERATOR));
+
+    let bodyObj = current_description.split(process.env.MSG_SEPERATOR);
+    body = bodyObj[0];
+    data = bodyObj[1] ?? "";
   }
-}
+
+  payload = buildPayload("Hello World");
+  console.log(payload);
 
 
-else {
-  console.log("Sorry, I don't know what to do.");
-}
+  core.setOutput('slack_payload', JSON.stringify(payload));
+  core.setOutput('pr_description', body);
+  core.setOutput('pr_description_data', data);
+// }
+
+
+// else {
+//   console.log("Sorry, I don't know what to do.");
+// }
 
 
 
@@ -39,4 +57,61 @@ try {
   core.setOutput('myOutput', myOutput);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+function buildPayload(statusMsg = "") {
+
+  let payload = {};
+  payload.blocks = [];
+
+  // Title.
+  let titleBlock = {};
+  titleBlock.type = "section";
+  titleBlock.text = {};
+  titleBlock.text.type = "mrkdwn";
+  let t = "";
+  if (CONTEXT_GITHUB.event.pull_request.draft) {
+    t += "draft ";
+  }
+  t += "PR #" + CONTEXT_GITHUB.event.number;
+  t += " | " + CONTEXT_GITHUB.event.pull_request.title;
+
+
+  titleBlock.text.text = "";
+
+  // Divider.
+  let divBlock = {};
+  divBlock.type = "divider";
+
+  // Current state.
+  let statusBlock = {};
+  statusBlock.type = "section";
+  statusBlock.text = {};
+  statusBlock.text.type = "mrkdwn";
+  statusBlock.text.text = statusMsg;
+
+  // Action w/ link.
+  let actionBlock = {};
+  actionBlock.type = "actions";
+  actionBlock.elements = [];
+
+  let actionElement = {};
+  actionElement.type = 'button';
+  actionElement.text = {
+    type: "plain_text",
+    text: "Goto PR",
+    emoji: true
+  };
+  actionElement.style = "primary";
+  actionElement.url = CONTEXT_GITHUB.event.pull_request.html_url;
+
+  actionBlock.elements.push(actionElement);
+
+  // Put it all together.
+  payload.blocks.push(titleBlock);
+  payload.blocks.push(divBlock);
+  payload.blocks.push(statusBlock);
+
+  return payload;
 }
